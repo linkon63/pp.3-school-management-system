baebbb172d7ec6b9c606b0e0374d002b8cbbb9ba
Md Aulad Hossain Anik <122743454+mdanik801@users.noreply.github.com>
2024-09-11 02:28:48 +0600

    Home Page Update

---
+++ src/components/landingpage/Campusfacilities.vue
@@ -0,0 +1,56 @@
+<script>
+export default {
+    data() {
+        return {
+            items: [
+                {
+                    name: 'Library',
+                    imgSrc: './landimg/Libary.jpg' // Path from the public folder
+                },
+                {
+                    name: 'Computer Lab',
+                    imgSrc: './landimg/computerlab.jpg' // Path from the public folder
+                },
+                {
+                    name: 'Science Lab',
+                    imgSrc: './landimg/sciencelaboratories.jpg' // Path from the public folder
+                },
+                {
+                    name: 'Music & Art Room',
+                    imgSrc: './landimg/musicroom.jpg' // Path from the public folder
+                },
+                {
+                    name: 'Auditorium',
+                    imgSrc: './landimg/aditorium.jpg' // Path from the public folder
+                },
+                {
+                    name: 'Conference Room',
+                    imgSrc: './landimg/conferenceroom.jpg' // Path from the public folder
+                },
+                {
+                    name: 'Transportation ',
+                    imgSrc: './landimg/transport.png' // Path from the public folder
+                },
+                {
+                    name: 'Playground',
+                    imgSrc: './landimg/playground.jpg' // Path from the public folder
+                }
+            ]
+        };
+    }
+};
+</script>
+
+<template>
+    <h1 class="lg:text-[2rem] md:text-[1.8rem] text-[1.4rem] text-center font-sans font-bold text-green-700 text-shadow-md m-4">Campus Facilities</h1>
+    <ul class="flex flex-wrap justify-center m-2 gap-4 text-[1.4rem] hover:text-red-600">
+        <li v-for="(item, index) in items" :key="index" class="max-w-sm rounded overflow-hidden shadow-md shadow-gray-500 relative hover:shadow-black duration-300">
+            <img class="lg:w-[30vh] lg:h-[20vh] sm:h-[12vh] w-[20vh] h-[15vh] object-cover hover:border duration-300" :src="item.imgSrc" :alt="item.name" />
+
+            <div class="absolute inset-0 bg-gray-300 opacity-60 rotate-45 lg:w-[50vh] w-[40vh]"></div>
+            <div class="absolute inset-0 flex justify-end p-2">
+                <h2 class="font-Archivo w-full text-end font-bold text-shadow text-black lg:text-[1.2rem] xl:text-[1rem] sm:text-[1.1rem] text-[0.8rem] hover:text-green-900 duration-300 bg-transparent">{{ item.name }}</h2>
+            </div>
+        </li>
+    </ul>
+</template>
---
+++ src/components/landingpage/Photogellary.vue
@@ -0,0 +1,39 @@
+<script setup>
+import Galleria from 'primevue/galleria';
+import { ref } from 'vue';
+
+const imgdata = ref([
+    {
+        image: './PhotoGalary/bookday.jpg'
+    },
+    {
+        image: './PhotoGalary/cultarprogram.jpg'
+    },
+    {
+        image: './PhotoGalary/21-February-5.jpg'
+    },
+    {
+        image: './PhotoGalary/16-december.jpg'
+    }
+]);
+
+// Map imgdata to create a new array that only includes image source and name
+const images = imgdata.value.map((item) => ({
+    imageSrc: item.image,
+    name: item.name
+}));
+</script>
+
+<template>
+    <div class="my-10">
+        <h1 class="lg:text-[2rem] md:text-[1.8rem] text-[1.4rem] text-center font-sans font-bold text-green-700 text-shadow-md m-4">Photo Gallery</h1>
+        <Galleria :value="images" :numVisible="5" :circular="true" :autoPlay="true" :transitionInterval="4000">
+            <template #item="slotProps">
+                <img :src="slotProps.item.imageSrc" :alt="slotProps.item.name" class="lg:w-[90vh] lg:h-[50vh] w-[40vh] h-[24vh] rounded-md" />
+            </template>
+            <template #thumbnail="slotProps">
+                <img :src="slotProps.item.imageSrc" :alt="slotProps.item.name" class="lg:w-[20vh] lg:h-[20vh] w-[10vh] h-[10vh]" />
+            </template>
+        </Galleria>
+    </div>
+</template>
